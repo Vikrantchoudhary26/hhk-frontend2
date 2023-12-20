115dfa2112218ec937c4068425f24e9fb204a637
akaleshtanwer <akalesh.aurasoft@gmail.com>
2023-12-20 22:28:31 +0530

    reverted base url issue

--- src/helpers/baseUrl.js
+++ src/helpers/baseUrl.js
@@ -1,2 +1,2 @@
-// export const api = "https://backend.hhkgifts.com/api";
-export const api = "http://localhost:5500/api";
+export const api = "https://backend.hhkgifts.com/api";
+// export const api = "http://localhost:5500/api";
